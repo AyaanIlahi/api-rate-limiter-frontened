88a90ca57fb631ca9dbb0e591ea42c2fb6d04bf8
Ayaan <146638318+AyaanIlahi@users.noreply.github.com>
2025-03-06 19:04:21 +0530

    Update fetchWithAuth.js with vercel url

--- app/api/utils/fetchWithAuth.js
+++ app/api/utils/fetchWithAuth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://192.168.1.74:8000";
+const API_BASE_URL = "https://api-rate-limiter-frontened.vercel.app";
 
 // Create an Axios instance
 const api = axios.create({
